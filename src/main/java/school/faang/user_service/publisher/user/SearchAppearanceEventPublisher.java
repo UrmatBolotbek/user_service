4cde2dc32c11e524385df6fad35b1b16034dbc83
IvanMatiko <matiko1993@gmail.com>
2024-12-08 23:07:52 +0100

    Made changes after crossreview

--- src/main/java/school/faang/user_service/publisher/user/SearchAppearanceEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/user/SearchAppearanceEventPublisher.java
@@ -1,32 +1,24 @@
 package school.faang.user_service.publisher.user;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.user.SearchAppearanceEvent;
+import school.faang.user_service.publisher.EventPublisherAbstract;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
-public class SearchAppearanceEventPublisher {
-
+public class SearchAppearanceEventPublisher extends EventPublisherAbstract<SearchAppearanceEvent> {
     @Value("${spring.data.redis.channels.search-appearance-channel}")
     private String topicSearchAppearanceEvent;
 
-    private RedisTemplate<String, Object> redisTemplate;
-    private ObjectMapper objectMapper;
+    public SearchAppearanceEventPublisher(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper) {
+        super(redisTemplate, objectMapper);
+    }
 
     public void publish(SearchAppearanceEvent event) {
-        try {
-            String json = objectMapper.writeValueAsString(event);
-            redisTemplate.convertAndSend(topicSearchAppearanceEvent, json);
-        } catch (JsonProcessingException e) {
-            log.error("An error occurred while working with JSON: ", e);
-            throw new RuntimeException(e);
-        }
+        handleEvent(event, topicSearchAppearanceEvent);
     }
 }
