c8ed93e60b4006c3b865e0f2db4fe70da7d9e431
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-12-11 23:15:01 +0300

    commit1 subscription to the project

--- src/main/java/school/faang/user_service/controller/subscription/ProjectFollowerController.java
+++ src/main/java/school/faang/user_service/controller/subscription/ProjectFollowerController.java
@@ -1,24 +1,25 @@
 package school.faang.user_service.controller.subscription;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.service.subscription.ProjectFollowerService;
-@Controller
+
+@RestController
+@RequestMapping("/api/v1/subscribe")
 @AllArgsConstructor
 @Slf4j
 public class ProjectFollowerController {
     private final ProjectFollowerService projectFollowerService;
     private final UserContext userContext;
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String>  subscribeToProject(
-            @PathVariable("projectId") Long projectId){
+
+    @PostMapping("/{projectId}/subscribe")
+    public ResponseEntity<String> subscribeToProject(@PathVariable("projectId") Long projectId) {
         Long userId = userContext.getUserId();
         log.info("User {} is subscribing to project {}", userId, projectId);
         projectFollowerService.subscribeToProject(userId, projectId);
